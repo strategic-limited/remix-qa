630638f225cfe8b3e81e34340d3c6082f3408a61
vedi <shubin.fedor@gmail.com>
2016-10-20 14:47:16 +0300

    refactoring
    complete TC003

--- tests/TC003_Playback_in_FB_tab_from_Post.js
+++ tests/TC003_Playback_in_FB_tab_from_Post.js
@@ -1,75 +1,42 @@
-var host = 'https://www.facebook.com/';
-module.exports =
-{
+'use strict';
 
-    'Playback in FB tab from Post': function(client)
-     {
-// Step 1 - open facebook and login.
-        client
-		    .windowMaximize()
-        .url(host)
-			  .waitForElementPresent("body" , 3000)
-			  .useXpath()
-        .setValue("//input[@id='email']","test15july@gmail.com")
-        .setValue("//input[@id='pass']","Abcdefgh@123")
-			  .waitForElementPresent("//input[@type='submit']" , 3000)
-        .click("//input[@type='submit']")
-        .waitForElementPresent("//div[@id='u_0_2']" , 4000)
+const config = require('../config/config');
+const specHelper = require('../lib/spec-helper');
 
-//Step 2 - Go to facebook post page
+module.exports = {
 
-        .execute(function(newWindow)
-  {
-            window.open('https://www.facebook.com/videoremixio/posts/1719151158354824', null, "height=1024,width=1524");
-  }, [host])
-        .window_handles(function(result)
-  {
-            var temp = result.value[1];
-            this.switchWindow(temp);
-        })
+  before(client) {
+    specHelper.prepareClient(client);
+    // Step 1 - open facebook and login.
+    specHelper.loginToFb(client, config.anotherFacebookAccount, true);
+  },
 
-        .useXpath()
-        .pause(5000)
-        .waitForElementVisible("//div[@id='js_n']", 10000)
-        .click("(//div[@class='_6l- __c_'])[1]")
-        .pause(5000)
-        .window_handles(function(result)
-        {
-            var temp = result.value[2];
-            this.switchWindow(temp);
-        })
-        .useCss()
-        .waitForElementVisible('body', 3000)
-        .useXpath()
-        .waitForElementVisible("//button[@name='__CONFIRM__']", 8000)
-        .click("//button[@name='__CONFIRM__']")
-        .pause(20000)
-        .execute('scrollTo(0,3000)')
+  'Playback in FB tab from Post'(client) {
+    //Step 2 - Go to facebook post page
+    const facebookPostPage = client.page.facebookPost();
+    facebookPostPage.navigate('https://www.facebook.com/videoremixio/posts/1719151158354824');
+    facebookPostPage.expect.element('@postLink').to.be.visible.before(5000);
+    facebookPostPage.openPost();
 
-        .frame(0, function() {
-         client
-        .waitForElementVisible("//span[@id='controls-play']", 3000)
-        .click("//span[@id='controls-play']")
-        .waitForElementVisible("//span[contains(text(),'0:09')]", 20000)
-        .click("//span[@id='controls-play']")
-        .pause(4000)
-        .click("//span[@id='controls-play']")
-        .pause(4000)
-        })
+    specHelper.switchToLastWindow(client);
+
+    facebookPostPage.expect.element('@confirmButton').to.be.visible.before(5000);
+    facebookPostPage.confirm();
+    facebookPostPage.expect.element('@videoFrame').to.be.visible.before(10000);
+
+    client.frame(0);
+    const playbackPage = client.page.playback();
+    playbackPage.expect.element('@playButton').to.be.visible.before(5000);
+    playbackPage.click('@playButton');
+    client.pause(4000);
+    playbackPage.click('@playButton');
+    client.pause(4000);
 
 // step 3 - navigate to facebook and cancel the VR App
+    specHelper.cancelAppInFb(client);
+  },
 
-        .url("https://www.facebook.com/settings?tab=applications")
-        .pause(10000)
-        .waitForElementVisible("//div[@id='u_1_0']", 5000)
-        .pause(7000)
-        .click("//div[@id='u_1_0']")
-        .waitForElementVisible("//a[contains(text(),'Remove App')]", 5000)
-        .click("//a[contains(text(),'Remove App')]")
-        .waitForElementVisible("//span[contains(text(),'Remove VidCloud.io?')]", 5000)
-        .click("//input[@name='ok']")
-        .pause(6000)
-        .end();
-
-      }
-  };
+  after(client) {
+    client.end();
+  }
+};

6ab764a6c8d43974fb83815570b65eadd891253c
Eugene Butusov <EvgenyButusov@gmail.com>
2017-02-15 11:51:03 +0300

    Check how to apply remix-qa module for existing stuff
    fix syntax for qa module

--- tests/extra/sample.js
+++ tests/extra/sample.js
@@ -4,9 +4,9 @@ module.exports = {
       .windowMaximize()
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
-      .setValue('//input[@id='email']', 'yvglrcw_wongwitz_1468002772@tfbnw.net')
-      .setValue('//input[@id='pass']', 'JPsRq5vMuzn')
-      .click('//input[@id='u_0_m']')
+      .setValue('//input[@id=\'email\']', 'yvglrcw_wongwitz_1468002772@tfbnw.net')
+      .setValue('//input[@id=\'pass\']', 'JPsRq5vMuzn')
+      .click('//input[@id=\'u_0_m\']')
       .pause(10000)
       .end();
   }
